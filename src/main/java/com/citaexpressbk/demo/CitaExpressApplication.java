08804327d031cc23f1b1739a2be9f7916d78747a
Simon Gonzalez <30642867+sgonzaec@users.noreply.github.com>
2024-11-26 05:12:00 +0000

    feat: update enviroment way

--- src/main/java/com/citaexpressbk/demo/CitaExpressApplication.java
+++ src/main/java/com/citaexpressbk/demo/CitaExpressApplication.java
@@ -8,11 +8,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CitaExpressApplication {
 
 	public static void main(String[] args) {
-		// Cargar las variables de entorno desde el archivo .env
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		String environment = System.getenv("ENVIRONMENT");
+		if (environment == null || !environment.equals("production")) {
+			// Solo carga .env si NO estás en producción
+			try {
+				// Cargar las variables de entorno desde el archivo .env
+				Dotenv dotenv = Dotenv.load();
+				System.setProperty("DB_URL", dotenv.get("DB_URL"));
+				System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+				System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+				System.out.println("Archivo .env cargado exitosamente.");
+			} catch (Exception e) {
+				System.out.println("Archivo .env no encontrado. Continuando con variables del sistema.");
+			}
+		} else {
+			System.out.println("Entorno de producción detectado. Usando variables del sistema.");
+		}
 
 		// Iniciar la aplicación Spring Boot
 		SpringApplication.run(CitaExpressApplication.class, args);
